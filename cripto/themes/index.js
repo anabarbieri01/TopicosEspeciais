e30fdd4408b7cf544bb1969a409e84e276666a5b
Ana Barbieri <ana.bbarbieri01@gmail.com>
2022-08-23 00:30:49 -0300

    1 passo - projeto cripto

---
+++ cripto/themes/index.js
@@ -0,0 +1,23 @@
+export default {
+    colors: {
+        brand:{
+            laranja : '#FFB86C',
+            azul: '#8BE9FD'
+        },
+
+        neutral: {
+            background: '#282A36',
+            foreground: '#44475A',
+            neutral100: '#CCCCCC',
+            neutral150: '#F8F8F2'
+        },
+
+        utility: {
+            success: '#50fa7b',
+            danger: '#ff5555',
+            warning: '#f1fa8c',
+            contrast: '#ff79c6',
+            info: '#6272a4'
+        }
+    }
+}
