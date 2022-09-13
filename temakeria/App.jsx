83e701f6dc3348c9ca1516eb8d9e56844304a67c
Ana Barbieri <ana.bbarbieri01@gmail.com>
2022-09-13 12:35:17 -0300

    Temakeria Finalizado

---
+++ temakeria/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import AppLoading from "expo-app-loading"
+import {useFonts, Ubuntu_400Regular, Ubuntu_500Medium,  Ubuntu_700Bold} from '@expo-google-fonts/ubuntu'
+import {Roboto_400Regular} from "@expo-google-fonts/roboto"
+import {View, Text} from 'react-native'
+import { Produto } from "./screens/Produto"
+
+export default function App(){
+  const [fontsLoaded] = useFonts({
+    Ubuntu_400Regular, Ubuntu_500Medium, Ubuntu_700Bold, Roboto_400Regular
+  })
+  if (!fontsLoaded){
+    return <AppLoading />
+  }
+  return (<Produto />)
+}
